6db7379e682ed1e39d48f35c81bcd701afcf2073
Bhavya Doshi <bhavyadoshi2006@gmail.com>
2026-01-07 13:02:17 +0530

    fix: update server configuration to listen on all network interfaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,6 @@ fi
 
 # Start the application
 echo "Starting application..."
-exec npx react-router-serve ./build/server/index.js
+# Use HOST=0.0.0.0 to listen on all network interfaces
+# Use PORT from environment variable or default to 3000
+HOST=0.0.0.0 PORT=${PORT:-3000} npx react-router-serve ./build/server/index.js
