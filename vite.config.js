13966c1a08947f624e69d0bcf9a1fc2d4d863e8e
Bhavya Doshi <bhavyadoshi2006@gmail.com>
2026-01-07 13:56:43 +0530

    Fix: tailwind problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
   import { reactRouter } from "@react-router/dev/vite";
   import { defineConfig } from "vite";
   import tsconfigPaths from "vite-tsconfig-paths";
-  import path from 'path';
 
   // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
   // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
@@ -38,14 +37,6 @@
   }
 
   export default defineConfig({
-    css: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
-    },
     server: {
       host: "0.0.0.0",                 // <- important for Render
       allowedHosts: [host],
