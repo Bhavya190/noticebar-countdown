bfe5d7ae191e271118cd8d4f0762bcf88777a97f
Bhavya Doshi <bhavyadoshi2006@gmail.com>
2026-01-07 12:42:44 +0530

    Fix: tailwind style adding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx,mdx}'],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.js",
+    "./app/**/*.jsx",
+    "./app/**/*.ts",
+    "./app/**/*.tsx",
+    "./app/**/*.html",
+    "./app/**/*.css"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
   import { reactRouter } from "@react-router/dev/vite";
   import { defineConfig } from "vite";
   import tsconfigPaths from "vite-tsconfig-paths";
+  import tailwindcss from 'tailwindcss';
+  import autoprefixer from 'autoprefixer';
 
   // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
   // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
@@ -37,6 +39,14 @@
   }
 
   export default defineConfig({
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
     server: {
       host: "0.0.0.0",                 // <- important for Render
       allowedHosts: [host],
